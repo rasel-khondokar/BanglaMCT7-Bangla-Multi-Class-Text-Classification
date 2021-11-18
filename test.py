d4da7779134bc2837fe6d998c6be207840dee43e
rasel-khondokar <mrrasel922@gmail.com>
2021-11-18 20:35:09 +0600

    test

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import pandas as pd
-#
-# from settings import DIR_DATASET
-#
-# data = pd.read_csv(DIR_DATASET + '/BanglaMCT7/train.csv')
-# state = data[data["category"] == 'state']
-# print(state)
 from configs import BASE_DIR
 
-data_file =  f'{BASE_DIR}/DATASET/prothomalo.json'
-df = pd.read_json(data_file)
-print(df.duplicated('url').sum())
+categories = {'sports': 'sports', 'international': 'world', 'economy': 'business', 'entertainment': 'entertainment',
+              'technology': 'education/science-tech', 'politics': 'politics', 'education': 'education'}
+
+
+for category in categories:
+    data_file =  f'{BASE_DIR}/DATASET/prothomalo_{category}.json'
+    df = pd.read_json(data_file)
+    print(f"{category}")
+    print(f"Total len :{len(df)}")
+    print(f"Duplicated :{df.duplicated('url').sum()}")
 
