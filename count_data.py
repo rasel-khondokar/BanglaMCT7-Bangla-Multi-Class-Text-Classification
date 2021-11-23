03771401d9af59aaa8c6e1542134eaf34cc6c268
rasel-khondokar <mrrasel922@gmail.com>
2021-11-24 00:16:28 +0600

    saved to csv

--- count_data.py
+++ count_data.py
@@ -27,3 +27,25 @@ for file in files:
             print("error")
 
 print(f"Total valid :{total-total_dup}")
+
+
+dataset = pd.DataFrame()
+dataset_dir = f'{BASE_DIR}/DATASET/'
+files = os.listdir(dataset_dir)
+for file in files:
+    if '.json' in file:
+        try:
+            data_file = dataset_dir + file
+            df = pd.read_json(data_file)
+            # time.sleep(10)
+            dataset = dataset.append(df, ignore_index=True)
+        except:
+            print(f"error during reading {file}")
+
+dataset.rename(columns={"raw_text": "cleanText"}, inplace=True)
+file = f'{dataset_dir}collected_data.csv'
+dataset.to_csv(file, index=False)
+
+df =  pd.read_csv(file)
+print(len(df))
+
