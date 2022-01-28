07c45e6aedbc9c47074785b36a082565cedca6a3
rasel-khondokar <mrrasel922@gmail.com>
2022-01-28 23:33:42 +0600

    education

--- scraping/multi_threding.py
+++ scraping/multi_threding.py
@@ -100,7 +100,7 @@ def scrape_banglanews24_from_recent_years():
    for year in years:
       years_q = f'category/education/20?y={year}&'
 
-      categories = {'technology':years_q}
+      categories = {'education':years_q}
 
       for category in categories:
          time.sleep(3)
