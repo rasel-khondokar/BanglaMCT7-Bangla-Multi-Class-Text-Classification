b9c116168c5f9897d03bf829916536b6df4ef270
rasel-khondokar <mrrasel922@gmail.com>
2022-05-31 15:27:45 +0600

    displayed freequency

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
-from bert import test_bert_model, run_bert_test
+from exploratory_analysis.eda import EDA
 from preprocessing.preprocessing import PreProcessor
-from settings import MODEL_BERT_MULTILANGUAL_CASED, DIR_RESOURCES
+from scraping.helpers import make_dir_if_not_exists
+from settings import DIR_REPORT, DIR_IMAGES_HISTORY, DIR_PERFORMENCE_REPORT, DIR_IMAGES_EDA
 
-models = {f'incorrect_{MODEL_BERT_MULTILANGUAL_CASED}': [MODEL_BERT_MULTILANGUAL_CASED.replace("/", "_"),
-                                                         MODEL_BERT_MULTILANGUAL_CASED]
-          }
+for dir in [DIR_REPORT, DIR_IMAGES_HISTORY, DIR_PERFORMENCE_REPORT, DIR_IMAGES_EDA]:
+    make_dir_if_not_exists(dir)
+name = 'incorrect_pred_removed'
+# get cleaned train and test data
 preprocessor = PreProcessor()
-df, df_test = preprocessor.read_collected_data_incorrect_pred_removed()
-run_bert_test(MODEL_BERT_MULTILANGUAL_CASED, df_test, is_test=False)
+data, data_test = preprocessor.read_collected_data_incorrect_pred_removed()
+
+# # exploratory data analysis
+eda = EDA(data, name)
+eda.visualize()
