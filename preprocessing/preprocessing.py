cc4859e0de5870d299870ab9532aab79cfe37f12
rasel-khondokar <mrrasel922@gmail.com>
2022-08-14 13:44:33 +0600

    all dl model with noisy data

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -301,15 +301,20 @@ class PreProcessor():
         file = f'{dataset_dir}collected_removed_urls_incorrect.csv'
         dataset = pd.read_csv(file)
         # dataset = dataset.sample(100)
-        file = f'{dataset_dir}removed_data.csv'
-        dataset_1 = pd.read_csv(file)
-        dataset = pd.concat([dataset, dataset_1])
+        # file = f'{dataset_dir}removed_data.csv'
+        # dataset_1 = pd.read_csv(file)
+        # dataset = pd.concat([dataset, dataset_1])
         dataset = dataset.drop_duplicates(subset=['url'])
         dataset = dataset[['cleanText', 'category']]
         dataset = dataset.reset_index()
         if is_split:
             data, data_test = train_test_split(dataset, test_size=.2, stratify=dataset.category.values)
-
+            file = f'{dataset_dir}removed_data.csv'
+            removed_data = pd.read_csv(file)
+            data = pd.concat([data, removed_data])
+            data = data.drop_duplicates(subset=['url'])
+            data = data[['cleanText', 'category']]
+            data = data.reset_index()
             # Remove null
             print(f'Before removing null Train data : {len(data)}')
             data.dropna(inplace=True)
